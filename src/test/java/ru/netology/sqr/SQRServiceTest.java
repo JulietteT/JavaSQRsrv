6ee4fd836a100c1b740258bab8353ce5fdc6e398
Юлия Ц <107360558+JulietteT@users.noreply.github.com>
2022-10-17 14:03:26 +0500

    Update SQRServiceTest.java

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,2 +1,18 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SQRServiceTest {
+
+    @Test
+    public void test() {
+        final SQRService service = new SQRService();
+        final int actual = service.calculate(200, 300);
+
+
+        final int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
